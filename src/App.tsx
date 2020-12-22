583a8d3b7fd155820c2fe4b9de3de4cb27542efd
Ben <benakehurst@hotmail.com>
2020-12-22 15:14:41 +0000

    header bar design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
+import { AppBar, Typography } from '@material-ui/core';
 import { Home } from './views/Home/Home';
 import { Saved } from './views/Saved/Saved';
 
@@ -10,14 +10,22 @@ function App() {
     <div className="App">
       <Router>
         <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/saved">Saved</Link>
-            </li>
-          </ul>
+          <AppBar position="static">
+            <div className="App_header__link-wrapper">
+              <Typography variant="h6">
+                <Link to="/" className="App_header__link">
+                  Home
+                </Link>
+              </Typography>
+            </div>
+            <div className="App_header__link-wrapper">
+              <Typography variant="h6">
+                <Link to="/saved" className="App_header__link">
+                  Saved
+                </Link>
+              </Typography>
+            </div>
+          </AppBar>
           <Switch>
             <Route exact path="/">
               <Home />
