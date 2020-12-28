af52cd4828207140767016faa328125aad5c4dd3
Ben <benakehurst@hotmail.com>
2020-12-28 19:00:32 +0000

    add images to posts and state hooks

--- src/components/Posts/Post/Post.tsx
+++ src/components/Posts/Post/Post.tsx
@@ -3,19 +3,31 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
+import './Post.scss';
 
-export const Post = (props: any) => (
-  <div>
-    <List component="nav" aria-label="single-post">
-      <ListItem
-        button
-        onClick={() => showPostItem(props.singlePostData.singlePostData.title)}
-      >
-        <ListItemText primary={props.singlePostData.singlePostData.title} />
-      </ListItem>
-    </List>
-    <Divider />
-  </div>
-);
+export const Post = (singlePostData: any) => {
+  return (
+    <div className={'singlePostWrapper'}>
+      <List component="nav" aria-label="single-post">
+        <ListItem
+          button
+          onClick={() => showPostItem(singlePostData.singlePostData.title)}
+        >
+          <ListItemText primary={singlePostData.singlePostData.title} />
+        </ListItem>
+      </List>
+      {singlePostData.singlePostData.url
+        .toLowerCase()
+        .match(/\.(jpg|png|gif)/g) ? (
+        <img
+          className={'singlePostImage'}
+          src={singlePostData.singlePostData.url}
+          alt={'post'}
+        />
+      ) : null}
+      <Divider />
+    </div>
+  );
+};
 
 const showPostItem = (postTitle: string) => {};
--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,30 +1,46 @@
-import React from 'react'; // we need this to make JSX compile
+import React, { useState } from 'react'; // we need this to make JSX compile
 import Button from '@material-ui/core/Button';
 import { PostItemType } from '../../types/Post.type';
 import { Post } from './Post/Post';
 import './Posts.scss';
 
-export const Posts = ({ defaultResults, chosenSubreddit }: any) => (
-  <div>
-    {chosenSubreddit && <h3>Current Subreddit: {chosenSubreddit}</h3>}
-    {defaultResults.map((singlePostItem: PostItemType) => {
-      return (
-        <div className={'singlePost'} key={singlePostItem.id}>
-          <Post singlePostData={singlePostItem} />
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ margin: '0.5rem 0.5rem' }}
-            onClick={() => savePost()}
-          >
-            Save Post
-          </Button>
-        </div>
-      );
-    })}
-  </div>
-);
+export const Posts = ({ defaultResults, chosenSubreddit }: any) => {
+  const [selectedUrl, setSelectedUrl] = useState('');
+  const [modalOpen, setModalOpen] = useState(false);
 
-const savePost = () => {
-  console.log('save post clicked');
+  const openPostPreview = (url: string) => {
+    console.log('url: ', url);
+  };
+
+  const savePost = () => {
+    console.log('save post clicked');
+  };
+
+  return (
+    <div>
+      {chosenSubreddit && <h3>Current Subreddit: {chosenSubreddit}</h3>}
+      {defaultResults.map((singlePostItem: PostItemType) => {
+        return (
+          <div key={singlePostItem.id} className={'singlePost'}>
+            <div
+              onClick={() => {
+                openPostPreview(singlePostItem.url);
+                setSelectedUrl(singlePostItem.url);
+              }}
+            >
+              <Post singlePostData={singlePostItem} />
+            </div>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ margin: '0.5rem 0.5rem' }}
+              onClick={() => savePost()}
+            >
+              Save Post
+            </Button>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
